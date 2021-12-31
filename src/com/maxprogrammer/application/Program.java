c04d8b340993860a5457d31c435be8b76a8bbf53
maxProgrammer <maxwneto.poo@gmail.com>
2021-12-31 12:41:52 -0300

    gerador de contrato

---
+++ src/com/maxprogrammer/application/Program.java
@@ -0,0 +1,49 @@
+package com.maxprogrammer.application;
+
+
+import com.maxprogrammer.models.entities.Contract;
+import com.maxprogrammer.models.entities.Installment;
+import com.maxprogrammer.models.services.ContractService;
+import com.maxprogrammer.models.services.OnlinePaymentService;
+import com.maxprogrammer.models.services.PaypalService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter contract data: ");
+        System.out.print("Number: ");
+        Integer number = sc.nextInt();
+        System.out.print("Date (dd/MM/yyyy): ");
+        Date date = sdf.parse(sc.next());
+        System.out.print("Contract value: ");
+        Double totalValue = sc.nextDouble();
+        System.out.print("Enter number of  installments: ");
+        int N = sc.nextInt();
+
+        Contract contract = new Contract(number, date, totalValue);
+
+        ContractService cs = new ContractService(new PaypalService());
+
+        cs.processContract(contract, N);
+
+        System.out.println("Intallments:");
+        for (Installment it : contract.getInstallments()) {
+            System.out.println(it);
+        }
+
+
+        sc.close();
+
+
+    }
+}
